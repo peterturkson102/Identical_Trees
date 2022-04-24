c66bfa05472dcfe4147b18a1074f48d0c9012ce8
peterturkson102 <peterturkson102@gmail.com>
2022-04-24 00:57:32 +0000

    added some comments

--- src/MAIN.java
+++ src/MAIN.java
@@ -37,13 +37,15 @@ class BinaryTree
     {
         BinaryTree tree = new BinaryTree();
 
+        //For tree 1
         tree.root1 = new Node(1);
         tree.root1.left = new Node(2);
         tree.root1.right = new Node(3);
         tree.root1.left.left = new Node(4);
         tree.root1.left.right = new Node(5);
 
-        tree.root2 = new Node(1);
+        //For Tree 2
+        tree.root2 = new Node(2);
         tree.root2.left = new Node(2);
         tree.root2.right = new Node(3);
         tree.root2.left.left = new Node(4);
@@ -52,7 +54,7 @@ class BinaryTree
         if (tree.identicalTrees(tree.root1, tree.root2))
             System.out.println("Both trees are identical");
         else
-            System.out.println("Trees are not identical");
+            System.out.println("The two Trees are not identical");
 
     }
 }q
